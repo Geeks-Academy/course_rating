c434bc16c052c049dec469d03d70782ee2231b5c
Kamil Grolik <kamilgrolik@macbook-pro-kamil.home>
2020-09-16 15:11:01 +0200

    Left menu initial

---
+++ admin/src/components/LeftMenu/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Wrapper } from "./styled";
+
+const LeftMenu = () => (
+  <Wrapper>
+    <a href="#">link 1</a>
+    <a href="#">link 2</a>
+  </Wrapper>
+);
+
+export default LeftMenu;
