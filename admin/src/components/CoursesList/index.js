7c63af1e876c598d9633edd62142f3a40205d97b
Michał Daniluk <forxik@gmail.com>
2020-09-17 21:33:10 +0200

    Add courses list to app and modify CourserList component

--- admin/src/components/CoursesList/index.js
+++ admin/src/components/CoursesList/index.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import { Wrapper } from './styled';
+import * as PropTypes from "prop-types";
 
-const CoursesList = () => <Wrapper>courses list</Wrapper>;
+const CoursesList = ({items}) => (
+    <Wrapper>
+        <ul>
+            {items.map(({name}) => <li>{name}</li>)}
+        </ul>
+    </Wrapper>
+);
+
+CoursesList.propTypes = {
+    items: PropTypes.arrayOf(PropTypes.shape({
+        name:PropTypes.string
+    }))
+}
+
+CoursesList.defaultProps = {
+    items:[],
+}
 
 export default CoursesList;
