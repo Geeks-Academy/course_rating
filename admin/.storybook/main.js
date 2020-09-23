9b10a487aafc2b6929c8a1423517757f8a2f6a04
Kamil Grolik <kamilgrolik@macbook-pro-kamil.home>
2020-09-23 20:52:39 +0200

    RAGC-41 - Storybook additional config

--- admin/.storybook/main.js
+++ admin/.storybook/main.js
@@ -5,4 +5,13 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/preset-create-react-app',
   ],
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    },
+  },
 };
--- admin/.storybook/preview.js
+++ admin/.storybook/preview.js
@@ -1,3 +1,8 @@
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
 };
+
+import React from 'react';
+import { addDecorator } from '@storybook/react';
+
+addDecorator((story) => <>{story()}</>);
