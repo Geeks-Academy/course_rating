9bd6c9663d252f96c050650af31ff8cdb8d3f5ef
Kromacie <kromacie@gmail.com>
2020-10-13 23:39:08 +0200

    RAGC-74 - Add categoryId suffix for course category endpoints

--- api/src/Controller/CourseController.php
+++ api/src/Controller/CourseController.php
@@ -110,17 +110,17 @@ class CourseController extends AbstractController
     }
 
     /**
-     * @Route("/{id}/categories", name="add_course_category", methods={"POST"})
+     * @Route("/{id}/categories/{categoryId}", name="add_course_category", methods={"POST"})
      */
-    public function addCourseCategory(Request $request, int $id)
+    public function addCourseCategory(Request $request, int $id, int $categoryId)
     {
         // TODO: Implement add course category endpoint
     }
 
     /**
-     * @Route("/{id}/categories", name="remove_course_category", methods={"DELETE"})
+     * @Route("/{id}/categories/{categoryId}", name="remove_course_category", methods={"DELETE"})
      */
-    public function removeCourseCategory(Request $request, int $id)
+    public function removeCourseCategory(Request $request, int $id, int $categoryId)
     {
         // TODO: Implement remove course category endpoint
     }
