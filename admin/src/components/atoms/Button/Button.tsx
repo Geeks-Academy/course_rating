b3a2164293816173b1487f4c1d14f747b433a3af
dziobakwszafie <dziobakwszafie@gmail.com>
2020-10-14 15:49:35 +0200

    RAGC-54 - Remove all icon logic and functions and optimize code in Button.styled.ts

--- admin/src/components/atoms/Button/Button.tsx
+++ admin/src/components/atoms/Button/Button.tsx
@@ -1,64 +1,22 @@
 import React from 'react';
 import { IButton } from './Button.model';
-import {
-  ButtonIcon,
-  DisableButton,
-  FillButton,
-  OutlineButton,
-  TransparentButton,
-} from './Button.styled';
+import { DisableButton, FillButton, OutlineButton, TransparentButton } from './Button.styled';
 
-const Button = ({
-  children,
-  className,
-  icon: Icon,
-  iconPos = null,
-  type,
-}: IButton): JSX.Element => {
+const Button = ({ children, className, type }: IButton): JSX.Element => {
   const content = () => {
-    if (Icon && iconPos) {
-      return (
-        <div>
-          <ButtonIcon type={type} iconPos={iconPos}>
-            <Icon />
-          </ButtonIcon>
-          <span>{children}</span>
-        </div>
-      );
-    }
     return <span>{children}</span>;
   };
   switch (type) {
     case 'outline':
-      return (
-        <OutlineButton data-testid="outline" className={className} iconPos={iconPos}>
-          {content()}
-        </OutlineButton>
-      );
+      return <OutlineButton className={className}>{content()}</OutlineButton>;
     case 'disabled':
-      return (
-        <DisableButton data-testid="disabled" className={className} iconPos={iconPos}>
-          {content()}
-        </DisableButton>
-      );
+      return <DisableButton className={className}>{content()}</DisableButton>;
     case 'transparent':
-      return (
-        <TransparentButton data-testid="transparent" className={className} iconPos={iconPos}>
-          {content()}
-        </TransparentButton>
-      );
+      return <TransparentButton className={className}>{content()}</TransparentButton>;
     case 'fill':
-      return (
-        <FillButton data-testid="fill" className={className} iconPos={iconPos}>
-          {content()}
-        </FillButton>
-      );
+      return <FillButton className={className}>{content()}</FillButton>;
     default:
-      return (
-        <FillButton data-testid="fill" className={className} iconPos={iconPos}>
-          {content()}
-        </FillButton>
-      );
+      return <FillButton className={className}>{content()}</FillButton>;
   }
 };
 
